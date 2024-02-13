99a8c781959c55f7f9bb46a0a236e7a3a19dca57
Tolga Özgün <tolgaozgunn@gmail.com>
2024-02-13 20:16:46 +0300

    Setup Spring Boot

---
+++ src/main/java/com/bilkent/devinsight/DevinsightApplication.java
@@ -0,0 +1,13 @@
+package com.bilkent.devinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevinsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevinsightApplication.class, args);
+	}
+
+}
